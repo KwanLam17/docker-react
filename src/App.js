2b04f3dcadad04a67ac7f10c674890ccd627aad1
KwanLam <kwan.lam@sap.com>
2019-03-20 11:22:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hi Kwan
           </a>
         </header>
       </div>
